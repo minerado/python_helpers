4dfdec9a58a0394568dfe8102995cd7fc98e38c0
Ramon <ramondiogo10.1@gmail.com>
2023-06-11 20:58:49 -0300

    add: accuracy function

--- torch.py
+++ torch.py
@@ -8,3 +8,8 @@ def print_train_time(start: float, end: float, device: torch.device = None):
     print(f"Train time on {device}: {total_time:.3f} seconds")
 
     return total_time
+
+
+def accuracy(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    return (correct / len(y_pred)) * 100
